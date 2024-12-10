c8d73627d17135ec37c4e8a397635dabfdfd90d9
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-12-09 20:30:58 -0330

    feat: Update component to use theme

--- src/components/ui/SwitchTextTrack.tsx
+++ src/components/ui/SwitchTextTrack.tsx
@@ -1,67 +1,72 @@
-import { styled } from "@mui/material/styles";
-import Switch, { switchClasses } from "@mui/material/Switch";
+import { styled, useTheme } from "@mui/material/styles";
+import Switch, { SwitchProps, switchClasses } from "@mui/material/Switch";
 
-export const SwitchTextTrack = styled(Switch)({
-  width: 80,
-  height: 48,
-  padding: 8,
-  [`& .${switchClasses.switchBase}`]: {
-    padding: 11,
-    color: "#ff6a00",
-  },
-  [`& .${switchClasses.thumb}`]: {
-    width: 26,
-    height: 26,
-    backgroundColor: "#fff",
-  },
-  [`& .${switchClasses.track}`]: {
-    background: "linear-gradient(to right, #FF0550, #FE7373)",
-    opacity: "1 !important",
-    borderRadius: 20,
-    position: "relative",
-    "&:before, &:after": {
-      display: "inline-block",
-      position: "absolute",
-      top: "50%",
-      width: "50%",
-      transform: "translateY(-50%)",
-      color: "#fff",
-      textAlign: "center",
-      fontSize: "0.75rem",
-      fontWeight: 500,
-    },
-    "&:before": {
-      content: '"YES"',
-      left: 4,
-      opacity: 0,
-    },
-    "&:after": {
-      content: '"NO"',
-      right: 4,
-    },
-  },
-  [`& .${switchClasses.checked}`]: {
-    [`&.${switchClasses.switchBase}`]: {
-      color: "#185a9d",
-      transform: "translateX(32px)",
-      "&:hover": {
-        backgroundColor: "rgba(24,90,257,0.08)",
-      },
+const SwitchTextTrack = (props: SwitchProps) => {
+  const theme = useTheme(); // Access the theme dynamically
+
+  const StyledSwitch = styled(Switch)({
+    width: 80,
+    height: 48,
+    padding: 8,
+    [`& .${switchClasses.switchBase}`]: {
+      padding: 11,
+      color: "#ff6a00",
     },
     [`& .${switchClasses.thumb}`]: {
+      width: 26,
+      height: 26,
       backgroundColor: "#fff",
     },
-    [`& + .${switchClasses.track}`]: {
-      background: "linear-gradient(to right, #FFC300, #FC5800)",
-      // background: "linear-gradient(to right, #00C8FF, #0050FC)",
-      "&:before": {
-        opacity: 1,
+    [`& .${switchClasses.track}`]: {
+      background: `linear-gradient(to right, ${theme.palette.error.light}, ${theme.palette.error.main})`,
+      opacity: "1 !important",
+      borderRadius: 20,
+      position: "relative",
+      "&:before, &:after": {
+        display: "inline-block",
+        position: "absolute",
+        top: "50%",
+        width: "50%",
+        transform: "translateY(-50%)",
+        color: "#ffffff",
+        textAlign: "center",
+        fontSize: "0.75rem",
+        fontWeight: 500,
       },
-      "&:after": {
+      "&:before": {
+        content: '"YES"',
+        left: 4,
         opacity: 0,
       },
+      "&:after": {
+        content: '"NO"',
+        right: 4,
+      },
     },
-  },
-});
+    [`& .${switchClasses.checked}`]: {
+      [`&.${switchClasses.switchBase}`]: {
+        color: "#185a9d",
+        transform: "translateX(32px)",
+        "&:hover": {
+          backgroundColor: "rgba(24,90,157,0.08)",
+        },
+      },
+      [`& .${switchClasses.thumb}`]: {
+        backgroundColor: "#fff",
+      },
+      [`& + .${switchClasses.track}`]: {
+        background: `linear-gradient(to right, ${theme.palette.orangeShade.light}, ${theme.palette.orangeShade.main})`,
+        "&:before": {
+          opacity: 1,
+        },
+        "&:after": {
+          opacity: 0,
+        },
+      },
+    },
+  });
 
-export default SwitchTextTrack
+  return <StyledSwitch {...props} />;
+};
+
+export default SwitchTextTrack;
