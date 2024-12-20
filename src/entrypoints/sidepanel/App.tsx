34c0cbbbe3a2c0fc6771e08883f94a4c25027fdc
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-12-20 15:59:19 -0330

    style: Lint sidepanel

--- src/entrypoints/sidepanel/App.tsx
+++ src/entrypoints/sidepanel/App.tsx
@@ -1,66 +1,73 @@
-import { useState, useEffect } from 'react';
-import imageUrl from '/icon/title-logo.png';
-import './App.css';
+import DeleteIcon from '@/components/ui/DeleteIcon'
+import HomeIcon from '@/components/ui/HomeIcon'
+
+import './App.css'
+
+import JargonCard from '@/components/ui/JargonCard'
+import ReportIcon from '@/components/ui/ReportIcon'
+import SettingsIcon from '@/components/ui/SettingsIcon'
+import BottomNavigation from '@mui/material/BottomNavigation'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import IconButton from '@mui/material/IconButton'
+import { ThemeProvider } from '@mui/material/styles'
+import { useEffect, useState } from 'react'
+
 import theme from '../../components/theme/theme'
-import { ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import ReportIcon from '@/components/ui/ReportIcon';
-import DeleteIcon from '@/components/ui/DeleteIcon';
-import HomeIcon from '@/components/ui/HomeIcon';
-import SettingsIcon from '@/components/ui/SettingsIcon';
-import Container from '@mui/material/Container';
-import JargonCard from '@/components/ui/JargonCard';
+
+import imageUrl from '/icon/title-logo.png'
 
 function App() {
   const instructionalEntry: Keyword = {
     keyword: 'get started',
-    type: 'instructions',
     meaning: 'select a complex block of text and right-click to simplify!',
-  };
-  const keywordStoreKey = 'keywordHistory';
-  const [keywordHistory, setKeywordHistory] = useState([instructionalEntry]);
+    type: 'instructions',
+  }
+  const keywordStoreKey = 'keywordHistory'
+  const [keywordHistory, setKeywordHistory] = useState([instructionalEntry])
 
   // Fetch keywordHistory from local storage on component mount
   useEffect(() => {
     const fetchKeywordHistory = async () => {
       try {
-        const keywordHistory: string | null = await storage.getItem(`local:${keywordStoreKey}`);
-        const keywordHistoryArray: Keyword[] | null = JSON.parse(keywordHistory || '[]');
-        const storedHistory = keywordHistoryArray || [];
-        setKeywordHistory([instructionalEntry, ...storedHistory]);
-      } catch (error) {
-        console.error('Error fetching keyword history:', error);
+        const keywordHistory: null | string = await storage.getItem(`local:${keywordStoreKey}`)
+        const keywordHistoryArray: Keyword[] | null = JSON.parse(keywordHistory || '[]')
+        const storedHistory = keywordHistoryArray || []
+        setKeywordHistory([instructionalEntry, ...storedHistory])
       }
-    };
+      catch (error) {
+        console.error('Error fetching keyword history:', error)
+      }
+    }
 
-    fetchKeywordHistory();
+    fetchKeywordHistory()
 
     const unwatch = storage.watch<number>(`local:${keywordStoreKey}`, () => {
-      fetchKeywordHistory();
-    });
+      fetchKeywordHistory()
+    })
 
     return () => {
-      unwatch();
+      unwatch()
     }
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const clearKeywordHistory = async () => {
     try {
-      await storage.setItem(`local:${keywordStoreKey}`, JSON.stringify([]));
-      setKeywordHistory([instructionalEntry]);
-    } catch (error) {
-      console.error('Error clearing keyword history:', error);
+      await storage.setItem(`local:${keywordStoreKey}`, JSON.stringify([]))
+      setKeywordHistory([instructionalEntry])
     }
-  };
+    catch (error) {
+      console.error('Error clearing keyword history:', error)
+    }
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <Container id="root">
         <header id="header">
-          <img draggable="false" user-select="none" src={imageUrl} alt="JarGone logo" className="logo" />
+          <img alt="JarGone logo" className="logo" draggable="false" src={imageUrl} user-select="none" />
           <span className="icon">
             <IconButton aria-label="contact support" href="mailto:overingowl+jargone+support@gmail.com">
               <ReportIcon />
@@ -72,15 +79,17 @@ function App() {
         </header>
 
         <Box className="content">
-          {keywordHistory.length === 0 ? (
-            <JargonCard keyword={instructionalEntry.keyword} type={instructionalEntry.type} meaning={instructionalEntry.meaning} />
-          ) : (
-            [...keywordHistory].reverse().map((entry, index) => (
-              <Box key={index} className='zoom-in'>
-                <JargonCard key={index} keyword={entry.keyword} type={entry.type} meaning={entry.meaning} />
-              </Box>
-            ))
-          )}
+          {keywordHistory.length === 0
+            ? (
+                <JargonCard keyword={instructionalEntry.keyword} meaning={instructionalEntry.meaning} type={instructionalEntry.type} />
+              )
+            : (
+                [...keywordHistory].reverse().map((entry, index) => (
+                  <Box className="zoom-in" key={index}>
+                    <JargonCard key={index} keyword={entry.keyword} meaning={entry.meaning} type={entry.type} />
+                  </Box>
+                ))
+              )}
         </Box>
 
         <Box id="bottom-nav">
@@ -89,20 +98,20 @@ function App() {
             value={0}
           >
             <BottomNavigationAction
-              className='.Mui-selected'
-              sx={{ borderRadius: 8, m: 1 }}
+              className=".Mui-selected"
               icon={<HomeIcon aria-label="home" color={theme.palette.primary.main} />}
+              sx={{ borderRadius: 8, m: 1 }}
             />
             <BottomNavigationAction
-              sx={{ borderRadius: 8, m: 1 }}
               icon={<SettingsIcon aria-label="settings" />}
               onClick={() => browser.runtime.openOptionsPage()}
+              sx={{ borderRadius: 8, m: 1 }}
             />
           </BottomNavigation>
         </Box>
       </Container>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
